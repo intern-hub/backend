a2396714b986c334cb1704b4b8c55cc2c4e005c6
David Hacker <dmhacker@protonmail.com>
2019-05-30 17:37:52 -0700

    Edit username-email access denied message to be more clear

--- src/main/java/com/internhub/backend/errors/exceptions/ForgotPasswordAccessDeniedException.java
+++ src/main/java/com/internhub/backend/errors/exceptions/ForgotPasswordAccessDeniedException.java
@@ -2,6 +2,6 @@ package com.internhub.backend.errors.exceptions;
 
 public class ForgotPasswordAccessDeniedException extends RuntimeException {
     public ForgotPasswordAccessDeniedException() {
-        super("Invalid username-email combination entered");
+        super("Invalid username or email entered");
     }
 }
